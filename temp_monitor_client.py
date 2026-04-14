eeba57e51db9468d078a9f7b61b3caa47d151c24
Tomás Rodriguez <tomasrour@unisabana.edu.co>
2026-04-14 14:46:55 -0500

    prueba 5 de temp_monitor_client.py y temp_monitor.py

--- temp_monitor_client.py
+++ temp_monitor_client.py
@@ -7,20 +7,20 @@ import temp_monitor
 
 def main():
     name = input("Nombre del archivo:")
-     # TODO: Pedir el nombre del archivo al usuario usando input()
+
     archivo = open(name, "r")
     n = int(archivo.readline().strip())
-    # TODO: Abrir el archivo y leer el numero de lecturas n
+
     monitor = temp_monitor.init(n)
-     # TODO: Crear el monitor usando temp_monitor.init(n)
+
     for _ in range(n):
         temp = float(archivo.readline().strip())
         monitor = temp_monitor.add_reading(monitor, temp)
     archivo.close()
-    # TODO: Leer las n temperaturas y agregarlas con temp_monitor.add_reading()
+    
     result = temp_monitor.longest_rising_streak(monitor)
     print (result)
-    # TODO: Imprimir la racha creciente mas larga usando temp_monitor.longest_rising_streak()
+    
     
     pass
 
